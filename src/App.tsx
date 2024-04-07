f45cd56d4791429b3e4cc84a770d36af6d91ce37
Maxim Grishaev <maxim.grishaev@gmail.com>
2024-04-07 20:36:20 +0200

    fix: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-import "./App.css";
-import Layout from "antd/es/layout";
-import { Content } from "antd/es/layout/layout";
-import MainRouter from "./Router/mainRouter";
-import Navbar from "./Components/common/navbar";
+import React from "react"
+import "./App.css"
+import Layout from "antd/es/layout"
+import { Content } from "antd/es/layout/layout"
+import MainRouter from "./Router/mainRouter"
 
 function App() {
   return (
-    <Layout className='layout' style={{ minHeight: "100vh" }}>
+    <Layout className="layout" style={{ minHeight: "100vh" }}>
       <Content>
         <MainRouter />
       </Content>
     </Layout>
-  );
+  )
 }
 
-export default App;
+export default App
